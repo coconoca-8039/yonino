7f60d1e332c3ee54e4314708e7d78a0b676a4361
coconoca-windows <coconoca.8039@gmail.com>
2025-05-17 23:25:31 +0900

    study module

---
+++ module.py
@@ -0,0 +1,14 @@
+#-----------------------------------------------------
+myvariable = "This is my variable"
+
+def myfunc():
+    print("This is my function")
+
+def yourfunc():
+    print("This is your func")
+
+def anotherfunc():
+    print("This is another")
+
+if __name__ == "__ main __":
+    print("This is module")
---
+++ module_main.py
@@ -0,0 +1,6 @@
+import module
+
+print(__name__)
+
+if __name__ == "__ main __":
+    print("This is main.py")
